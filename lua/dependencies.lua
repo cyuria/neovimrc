18e05261c93aa394fb32442f568b03f5db1009b6
Cyuria <55673467+Cyuria@users/noreply.github.com>
2024-02-09 13:14:19 +1100

    fix previous commit

--- lua/dependencies.lua
+++ lua/dependencies.lua
@@ -1,5 +1,7 @@
+local version = vim.version()
+
 return {
-    enable_version = vim.version().major >= 1 or vim.version.minor() >= 10,
+    enable_version = version.major >= 1 or version.minor >= 10,
     enable_github = vim.fn.executable 'gh' == 1,
     enable_discord = vim.fn.executable 'discord' == 1,
 }
